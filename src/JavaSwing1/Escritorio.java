7acdc81b05605a3ed1d4b8a916cc0d8f29de2915
inescastillo1968 <inesmayo12345@hotmail.com>
2017-11-08 20:54:02 -0300

    Importando las librerias

---
+++ src/JavaSwing1/Escritorio.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaSwing1;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author LAB10
+ */
+public class Escritorio extends JFrame {
+    
+}
