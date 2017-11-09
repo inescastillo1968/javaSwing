ca91ce14db3414c278babf4458d5f0dc67214de3
inescastillo1968 <inesmayo12345@hotmail.com>
2017-11-08 21:24:07 -0300

    crear elementos jframe

--- src/JavaSwing1/Escritorio.java
+++ src/JavaSwing1/Escritorio.java
@@ -19,4 +19,22 @@ public class Escritorio extends JFrame
    JButton boton; 
 JCheckBox verificacion;
 JRadioButton presione;
+public Escritorio()
+{
+  panel = new JPanel ();
+     label = new JLabel();
+     text = new JTextField(20);
+     boton = new JButton();
+     verificacion = new JCheckBox();
+     presione = new JRadioButton();
+     
+     this.add(panel);//recomendable agregar el JPanel al JFrame
+    panel.add(label);//agrego los elementos al JPanel
+    panel.add(text);
+    panel.add(boton);
+}
+
+
+
+
 }
